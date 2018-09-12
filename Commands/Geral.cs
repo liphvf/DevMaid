5ddd8c70914f957f301c1e05f459b414a093c3a3
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2018-09-12 20:55:07 -0300

    Clean ++

--- Commands/Geral.cs
+++ Commands/Geral.cs
@@ -262,24 +262,6 @@ namespace DevMaid.Commands
             }
         }
 
-        public static void CombineMultipleFilesIntoSingleFile(string inputDirectoryPath, string inputFileNamePattern, string outputFilePath = "./")
-        {
-            string[] inputFilePaths = Directory.GetFiles(inputDirectoryPath, inputFileNamePattern);
-            Console.WriteLine("Number of files: {0}.", inputFilePaths.Length);
-            using (var outputStream = File.Create(outputFilePath))
-            {
-                foreach (var inputFilePath in inputFilePaths)
-                {
-                    using (var inputStream = File.OpenRead(inputFilePath))
-                    {
-                        // Buffer size can be passed as the second argument.
-                        inputStream.CopyTo(outputStream);
-                    }
-                    Console.WriteLine("The file {0} has been processed.", inputFilePath);
-                }
-            }
-        }
-
         public static void CombineMultipleFilesIntoSingleFile(string inputDirectoryPathWithPattern, string outputFilePath = null)
         {
             if (outputFilePath == null)
@@ -294,6 +276,7 @@ namespace DevMaid.Commands
 
             var inputFilePaths = Directory.GetFiles(directory, pattern);
             Console.WriteLine("Number of files: {0}.", inputFilePaths.Length);
+
             if (!inputFilePaths.Any())
             {
                 throw new Exception("Files not Found");
@@ -304,12 +287,9 @@ namespace DevMaid.Commands
                 currentEncoding = GetCurrentFileEncoding(inputFilePath);
                 GetAllFileText = File.ReadAllText(inputFilePath, currentEncoding);
 
-                // Console.WriteLine(GetAllFileText);
-
                 Console.WriteLine("The file {0} has been processed.", inputFilePath);
             }
-            Console.WriteLine($"outputFilePath: {outputFilePath}");
-            Console.WriteLine($"currentEncoding: {currentEncoding}");
+
             File.WriteAllText(outputFilePath, $"{GetAllFileText}{Environment.NewLine}", currentEncoding);
         }
 
